a3a80a268e5dd55faf2fab384c5e9a64d9c9a191
jiyinyiyong <jiyinyiyong@gmail.com>
2020-08-16 00:45:38 +0800

    very little code trying to add initial adding button

--- src/app/comp/navigation.cljs
+++ src/app/comp/navigation.cljs
@@ -3,26 +3,39 @@
   (:require [hsl.core :refer [hsl]]
             [respo-ui.core :as ui]
             [respo.comp.space :refer [=<]]
-            [respo.core :refer [defcomp <> span div]]
-            [app.config :as config]))
+            [respo.core :refer [defcomp >> <> span div button]]
+            [clojure.string :as string]
+            [app.config :as config]
+            [respo-alerts.core :refer [use-prompt]]))
 
 (defcomp
  comp-navigation
- (logged-in? count-members)
- (div
-  {:style (merge
-           ui/row-center
-           {:height 48,
-            :justify-content :space-between,
-            :padding "0 16px",
-            :font-size 16,
-            :border-bottom (str "1px solid " (hsl 0 0 0 0.1)),
-            :font-family ui/font-fancy})}
-  (div
-   {:on-click (fn [e d!] (d! :router/change {:name :home})), :style {:cursor :pointer}}
-   (<> (:title config/site) nil))
-  (div
-   {:style {:cursor "pointer"}, :on-click (fn [e d!] (d! :router/change {:name :profile}))}
-   (<> (if logged-in? "Me" "Guest"))
-   (=< 8 nil)
-   (<> count-members))))
+ (states logged-in? count-members)
+ (let [add-question (use-prompt (>> states :new) {:title "New question"})]
+   (div
+    {:style (merge
+             ui/row-center
+             {:height 48,
+              :justify-content :space-between,
+              :padding "0 16px",
+              :font-size 16,
+              :border-bottom (str "1px solid " (hsl 0 0 0 0.1)),
+              :font-family ui/font-fancy})}
+    (div
+     {:on-click (fn [e d!] (d! :router/change {:name :home})), :style {:cursor :pointer}}
+     (<> (:title config/site) nil)
+     (=< 8 nil)
+     (button
+      {:style ui/button,
+       :inner-text "Question",
+       :on-click (fn [e d!]
+         ((:show add-question)
+          d!
+          (fn [text] (when-not (string/blank? text) (d! :question/add text)))))}))
+    (div
+     {:style {:cursor "pointer"},
+      :on-click (fn [e d!] (d! :router/change {:name :profile}))}
+     (<> (if logged-in? "Me" "Guest"))
+     (=< 8 nil)
+     (<> count-members))
+    (:ui add-question))))
--- src/app/schema.cljs
+++ src/app/schema.cljs
@@ -1,6 +1,18 @@
 
 (ns app.schema )
 
+(def reply {:id nil, :time nil, :content nil, :related-ids {}, :author-id nil})
+
+(def question
+  {:id nil,
+   :title nil,
+   :time nil,
+   :author-id nil,
+   :description nil,
+   :inspiration-id nil,
+   :related-ids #{},
+   :replies (do reply {})})
+
 (def router {:name nil, :title nil, :data {}, :router nil})
 
 (def session
@@ -12,4 +24,4 @@
 
 (def user {:name nil, :id nil, :nickname nil, :avatar nil, :password nil})
 
-(def database {:sessions (do session {}), :users (do user {})})
+(def database {:sessions (do session {}), :users (do user {}), :question (do question {})})
